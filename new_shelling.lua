5696e0c325337732ed8bbd4d110bd6db1673e543
BubbaFrog <100529581+BubbaFrog@users.noreply.github.com>
2022-08-22 21:14:23 -0400

    Add files via upload

---
+++ new_shelling.lua
@@ -0,0 +1,11 @@
+
+
+minetest.register_craftitem("rangedweapons:shell_incendiarydrop", {
+	wield_scale = {x=2.5,y=1.5,z=1.0},
+	inventory_image = "rangedweapons_shelldrop_incendiary.png",
+})
+
+minetest.register_craftitem("rangedweapons:shell_nothingdrop", {
+	wield_scale = {x=2.5,y=1.5,z=1.0},
+	inventory_image = "rangedweapons_shelldrop_nothing.png",
+})
