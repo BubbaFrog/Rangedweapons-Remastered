b561ea8c422500182cc2caa0035f4d84f9a9643c
BubbaFrog <100529581+BubbaFrog@users.noreply.github.com>
2023-07-27 18:45:30 -0400

    Add files via upload

--- infinitygauntlet.lua
+++ infinitygauntlet.lua
@@ -1,39 +1,63 @@
-	minetest.register_tool("rangedweapons:infinitygauntlet", {
+minetest.register_tool("rangedweapons:infinitygauntlet_rld", {
 	stack_max= 1,
-	wield_scale = {x=2.0,y=2.0,z=1.75},
-		description = "" ..core.colorize("#35cdff","Infinity Gauntlet\n") ..core.colorize("#FFFFFF", "Ranged damage: 10\n") ..core.colorize("#FFFFFF", "accuracy: 40%\n") ..core.colorize("#FFFFFF", "projectiles: 6\n") ..core.colorize("#FFFFFF", "knockback: 0\n")  ..core.colorize("#FFFFFF", "Critical chance: 8%\n") ..core.colorize("#FFFFFF", "Critical efficiency: 2.2x\n")  ..core.colorize("#FFFFFF", "Power usage: 30\n") ..core.colorize("#FFFFFF", "Rate of fire: 0.5\n") ..core.colorize("#FFFFFF", "Enemy penetration: 40%\n") ..core.colorize("#FFFFFF", "Gun type: power pistol\n") ..core.colorize("#FFFFFF", "Bullet velocity: 55"),
 	range = 0,
+	wield_scale = {x=1.9,y=1.9,z=1.1},
+	description = "",
+	loaded_gun = "rangedweapons:infinitygauntlet",
+	loaded_sound = "rangedweapons_magic2",
+	groups = {not_in_creative_inventory = 1},
 	inventory_image = "rangedweapons_infinitygauntlet.png",
-RW_powergun_capabilities = {
-		power_damage = {fleshy=10,knockback=0},
-		power_crit = 8,
-		power_critEffc = 2.2,
-		power_skill = {"",1},
-		power_cooling = "rangedweapons:infinitygauntlet",
-		power_velocity = 55,
-		power_accuracy = 40,
-		power_cooldown = 0.5,
-		power_projectiles = 1,
-		power_durability = 2000,
-		power_sound = "rangedweapons_needle",
-		power_glass_breaking = 1,
-		power_door_breaking = 1,
-		power_dps = 0,
-		power_mob_penetration = 40,
-		power_node_penetration = 0,
-		power_dps = 0,
-		power_consumption = 30,
-		power_entity = "rangedweapons:shot_bullet",
-		power_visual = "sprite",
-		power_texture = "rangedweapons_dogray.png",
-		power_projectile_size = 0.005,
-		power_projectiles = 6,
-		has_sparks = 0,
-		ignites_explosives = 1,
+})
+
+minetest.register_tool("rangedweapons:infinitygauntlet", {
+		description = "" ..core.colorize("#35cdff","The Infinity Gauntley use only if you have admin because you can die from this weapon\n") ..core.colorize("#FFFFFF", "Ranged damage: 3\n") ..core.colorize("#FFFFFF", "projectiles: 6\n") ..core.colorize("#FFFFFF", "Gun gravity: 3\n") ..core.colorize("#FFFFFF", "Accuracy: 52%\n")..core.colorize("#FFFFFF", "knockback: 7\n") ..core.colorize("#FFFFFF", "Critical chance: 7%\n") ..core.colorize("#FFFFFF", "Critical efficiency: 2.1x\n") ..core.colorize("#FFFFFF", "Ammunition: 40mm\n") ..core.colorize("#FFFFFF", "Pump delay: 0.45\n")..core.colorize("#FFFFFF", "Clip size: 4\n") ..core.colorize("#27a600", "Gun is ready to fire!\n") ..core.colorize("#fff21c", "Right-click to load in a bullet!\n")  ..core.colorize("#FFFFFF", "Gun type: Magic\n") ..core.colorize("#FFFFFF", "Bullet velocity: 32"),
+	range = 0,
+	wield_scale = {x=1.9,y=1.9,z=1.1},
+	inventory_image = "rangedweapons_infinitygauntlet.png",
+RW_gun_capabilities = {
+		gun_damage = {fleshy=3,knockback=7},
+		gun_crit = 7,
+		gun_critEffc = 2.1,
+		suitable_ammo = {{"rangedweapons:40mm",5}},
+		gun_skill = {"shotgun_skill",20},
+		gun_icon = "rangedweapons_infinitygauntlet.png",
+		gun_unloaded = "rangedweapons:infinitygauntlet_rld",
+		gun_cooling = "rangedweapons:infinitygauntlet_uld",
+		gun_velocity = 32,
+		gun_accuracy = 52,
+		gun_cooldown = 0.45,
+		gun_gravity = 3,
+		gun_reload = 0.25,
+		gun_projectiles = 6,
+		has_shell = 0,
+		gun_durability = 550,
+		gun_smokeSize = 15,
+		gun_door_breaking = 1,
+		gun_sound = "rangedweapons_fingersnap",
+		gun_unload_sound = "rangedweapons_magic",
 	},
+	on_secondary_use = function(itemstack, user, pointed_thing)
+rangedweapons_single_load_gun(itemstack, user, "")
+return itemstack
+end,
 	on_use = function(itemstack, user, pointed_thing)
-rangedweapons_shoot_powergun(itemstack, user)
+rangedweapons_shoot_gun(itemstack, user)
 return itemstack
 	end,
+})
 
+minetest.register_tool("rangedweapons:infinitygauntlet_uld", {
+	stack_max= 1,
+	wield_scale = {x=1.9,y=1.9,z=1.1},
+	range = 0,
+		description = "" ..core.colorize("#35cdff","The Infinity Gauntley\n") ..core.colorize("#FFFFFF", "Ranged damage: 2\n") ..core.colorize("#FFFFFF", "projectiles: 6\n") ..core.colorize("#FFFFFF", "Gun gravity: 3\n") ..core.colorize("#FFFFFF", "Accuracy: 52%\n")..core.colorize("#FFFFFF", "knockback: 7\n") ..core.colorize("#FFFFFF", "Critical chance: 7%\n") ..core.colorize("#FFFFFF", "Critical efficiency: 2.1x\n") ..core.colorize("#FFFFFF", "Ammunition: 40mm and rockets\n") ..core.colorize("#FFFFFF", "Pump delay: 0.45\n")..core.colorize("#FFFFFF", "Clip size: 4/4\n") ..core.colorize("#be0d00", "Right-click, to eject the empty shell!\n") ..core.colorize("#fff21c", "Right-click to load in a bullet!\n")  ..core.colorize("#FFFFFF", "Gun type: magic\n") ..core.colorize("#FFFFFF", "Bullet velocity: 28"),
+	inventory_image = "rangedweapons_infinitygauntlet.png",
+	groups = {not_in_creative_inventory = 1},
+	on_use = function(user)
+		minetest.sound_play("rangedweapons_empty", {user})
+	end,
+on_secondary_use = function(itemstack, user, pointed_thing)
+eject_shell(itemstack,user,"rangedweapons:infinitygauntlet_rld",0.6,"rangedweapons_none","rangedweapons:empty_shell")
+return itemstack
+end,
 })
