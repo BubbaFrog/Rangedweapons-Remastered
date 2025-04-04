24a05799cc38549c35fc2fd5e01907cbd7c0f7da
BubbaFrog <100529581+BubbaFrog@users.noreply.github.com>
2025-04-04 01:29:15 -0400

    Delete desertcamo.lua

--- desertcamo.lua
+++
@@ -1,4 +0,0 @@
-
-local MP = minetest.get_modpath("rangedweapons")
-
-dofile(MP.."/daa12.lua")                                                                                                                                           dofile(MP.."/dsniper.lua")                                                                                                                                    dofile(MP.."/dmilkor.lua")                                                                                                                                               dofile(MP.."/dak47.lua")                                                                                                                                     
